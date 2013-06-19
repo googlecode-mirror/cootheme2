c137a930b820267629ed9d1be66e09833853a21a
quanglemon <quanglemon@gmail.com>
2013-06-19 08:44:23 +0000

    update models

--- app/models/CommentModel.php
+++ app/models/CommentModel.php
@@ -12,7 +12,7 @@
 class CommentModel extends  Coo_Mvc_Model_BaseModel {
 
     public function __construct(){
-
+        parent::__construct();
     }
 
     public function get($args) {
@@ -86,102 +86,4 @@ class CommentModel extends  Coo_Mvc_Model_BaseModel {
     public function countComment($postId){
         return get_comments_number($postId);
     }
-
-
-
-
-    /*
-    public function getById($id){
-        $comment = get_comment($id);
-        if(is_object($comment)){
-            return new Comment(get_object_vars($comment));
-        }else{
-            return null;
-        }
-    }
-
-    public function getCommentByPost($post_id ,$number = '', $args = array()){
-        $args['post_id'] = $post_id;
-        $args['number'] = $number;
-        $comments = get_comments($args);
-        if(is_array($comments)){
-            $result = array();
-            foreach($comments as $comment){
-                $result[] = new Comment(get_object_vars($comment));
-            }
-            return $result;
-        }else{
-            return null;
-        }
-    }
-
-    public function getRecentComment($number = 10, $conditions = array('status'=>'approve')){
-        $coditions['number'] = $number;
-        $comments = get_comments($args);
-        if(is_array($comments)){
-            $result = array();
-            foreach($comments as $comment){
-                $result[] = new Comment(get_object_vars($comment));
-            }
-            return $result;
-        }else{
-            return null;
-        }
-    }
-
-
-
-    public function getCommentByUser($user_id, $number = '', $args = array()){
-        $args['user_id'] = $user_id;
-        $args['number'] = $number;
-        $comments = get_comments($args);
-        if(is_array($comments)){
-            $result = array();
-            foreach($comments as $comment){
-                $result[] = new Comment(get_object_vars($comment));
-            }
-            return $result;
-        }else{
-            return null;
-        }
-    }
-
-    public function getCommentAuthor($comment_id){
-        return get__comment_author($comment_id);
-    }
-
-    public function getCommentNumber($post_id){
-        return get_comments_number($post_id);
-    }
-
-    public function insert($comment_data){
-        return wp_insert_comment($comment_data);
-    }
-
-    public function update($comment_data){
-        return wp_update_comment($comment_data);b
-    }
-
-    public function delete($comment_id, $force='false'){
-        return wp_delete_comment($comment_id, $force='false');
-    }
-
-
-
-    public function doQuery($query_string){
-        global $wpdb;
-        $rows = $wpdb->get_results($query_string, OBJECT);
-        if(is_array($rows)){
-            $result = array();
-            foreach($rows as $row){
-                $result[] = new Comment(get_object_vars($row));
-            }
-            return $result;
-        }else{
-            return null;
-        }
-    }
-
-    */
-
 }
