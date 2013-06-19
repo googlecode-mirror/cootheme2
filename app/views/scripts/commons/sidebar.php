d7e74a0f565e5a1812688116c7188773a191d406
khangvm530 <khangvm530@gmail.com>
2013-06-19 10:08:49 +0000

    Commit commons script

--- app/views/scripts/commons/sidebar.php
+++ app/views/scripts/commons/sidebar.php
@@ -1,4 +1,10 @@
-        <div id="sidebar">sidebar</div>
+            <div class="span3">
+                <div id="sidebar">sidebar</div>
 
-        <div style="clear: left"><span></span></div>
+                <div style="clear: left"><span></span></div>
+            </div><!-- end of span3 -->
+        </row> <!--end of row -->
+</div><!-- end span10-->
+</div>  <!-- end of row-->
+
 
